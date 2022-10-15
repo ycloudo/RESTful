4473a9f712a7cd5ec787cb3cecf11162179fcd2a
ycloudo <cces994512@gmail.com>
2022-10-15 12:49:32 +0800

    all info api fixed

--- src/controller/RestaurantController.js
+++ src/controller/RestaurantController.js
@@ -39,6 +39,7 @@ const AllInfo = async (req, res) => {
                     class_rate: a.class_rate,
                 };
             });
+            res.status(200).json(result);
         });
     } catch (err) {
         res.status(400).json({ message: err });
