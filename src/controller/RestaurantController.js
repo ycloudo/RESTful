ba73882e67473ac3fda42401878c90d017f1fbac
ycloudo <cces994512@gmail.com>
2022-10-15 11:42:42 +0800

    add favor-info api fixed

--- src/controller/RestaurantController.js
+++ src/controller/RestaurantController.js
@@ -39,9 +39,6 @@ const AllInfo = async (req, res) => {
                     class_rate: a.class_rate,
                 };
             });
-            if (index == 30) {
-                res.status(200).json(result);
-            }
         });
     } catch (err) {
         res.status(400).json({ message: err });
