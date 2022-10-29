3ddc1cd48e56ee527091eb51feb5561bf8cd67a7
ycloudo <cces994512@gmail.com>
2022-10-29 14:48:30 +0800

    fix fetch all restaurant api

--- src/controller/RestaurantController.js
+++ src/controller/RestaurantController.js
@@ -1,102 +1,102 @@
-import Restaurant from "../model/Restaurant.js";
+import Restaurant from '../model/Restaurant.js';
 
 const ResInfo = async (req, res) => {
-    const id = req.params.rid;
-    let i = 0;
-    try {
-        const restaurant = await Restaurant.findOne({
-            _id: id,
-        });
-        let reviews = restaurant.reviews;
-        reviews.forEach((e) => {
-            const array = e.type;
-            const result = array.filter((item, index, array) => {
-                return array.indexOf(item) === index;
-            });
-            reviews[i++].type = result;
-        });
-        res.status(200).json({
-            reviews: restaurant.reviews,
-        });
-    } catch (err) {
-        res.status(400).json({ message: err });
-    }
+  const id = req.params.rid;
+  let i = 0;
+  try {
+    const restaurant = await Restaurant.findOne({
+      _id: id,
+    });
+    let reviews = restaurant.reviews;
+    reviews.forEach((e) => {
+      const array = e.type;
+      const result = array.filter((item, index, array) => {
+        return array.indexOf(item) === index;
+      });
+      reviews[i++].type = result;
+    });
+    res.status(200).json({
+      reviews: restaurant.reviews,
+    });
+  } catch (err) {
+    res.status(400).json({ message: err });
+  }
 };
 
 const AllInfo = async (req, res) => {
-    let result = [];
-    let index = 0;
-    const page = req.params.page;
-    try {
-        const rest = await Restaurant.find({})
-            .limit(page * 10)
-            .skip((page - 1) * 10);
-        rest.forEach((a) => {
-            result[index++] = {
-                id: a._id,
-                name: a.name,
-                rate: a.rate,
-                address: a.address,
-                res_type: a.restaurant_type,
-                photo: a.photo,
-                class_rate: a.class_rate,
-            };
-        });
-        res.status(200).json(result);
-    } catch (err) {
-        res.status(400).json({ message: err });
-    }
+  let result = [];
+  let index = 0;
+  const page = req.params.page;
+  try {
+    const rest = await Restaurant.find({})
+      .limit(10)
+      .skip((page - 1) * 10);
+    rest.forEach((a) => {
+      result[index++] = {
+        id: a._id,
+        name: a.name,
+        rate: a.rate,
+        address: a.address,
+        res_type: a.restaurant_type,
+        photo: a.photo,
+        class_rate: a.class_rate,
+      };
+    });
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(400).json({ message: err });
+  }
 };
 
 const infoById = async (req, res) => {
-    let result = [];
-    let index = 0;
-    const favors = req.body.favors;
-    try {
-        for (let i = 0; i < favors.length; i++) {
-            const res = await Restaurant.findOne({
-                _id: favors[i],
-            });
-            result[index++] = {
-                id: res._id,
-                name: res.name,
-                rate: res.rate,
-                address: res.address,
-                res_type: res.restaurant_type,
-                photo: res.photo,
-                class_rate: res.class_rate,
-            };
-        }
-        res.status(200).json(result);
-    } catch (err) {
-        res.status(400).json({ message: err });
+  let result = [];
+  let index = 0;
+  const favors = req.body.favors;
+  try {
+    for (let i = 0; i < favors.length; i++) {
+      const res = await Restaurant.findOne({
+        _id: favors[i],
+      });
+      result[index++] = {
+        id: res._id,
+        name: res.name,
+        rate: res.rate,
+        address: res.address,
+        res_type: res.restaurant_type,
+        photo: res.photo,
+        class_rate: res.class_rate,
+      };
     }
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(400).json({ message: err });
+  }
 };
 
 const infoByTag = async (req, res) => {
-    const cid = req.params.cid;
-    const page = req.params.page;
-    let result = [];
-    let index = 0;
-    try {
-        const rest = await Restaurant.find({ restaurant_type: cid })
-            .limit(page * 10)
-            .skip((page - 1) * 10);
-        rest.forEach((a) => {
-            result[index++] = {
-                id: a._id,
-                name: a.name,
-                rate: a.rate,
-                address: a.address,
-                res_type: a.restaurant_type,
-                photo: a.photo,
-                class_rate: a.class_rate,
-            };
-        });
-        res.status(200).json(result);
-    } catch (err) {
-        res.status(400).json({ message: err });
-    }
+  const cid = req.params.cid;
+  const page = req.params.page;
+  let result = [];
+  let index = 0;
+  try {
+    const rest = await Restaurant.find({ restaurant_type: cid })
+      .limit(page * 10)
+      .skip((page - 1) * 10);
+    rest.forEach((a) => {
+      result[index++] = {
+        id: a._id,
+        name: a.name,
+        rate: a.rate,
+        address: a.address,
+        res_type: a.restaurant_type,
+        photo: a.photo,
+        class_rate: a.class_rate,
+      };
+    });
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(400).json({ message: err });
+  }
 };
 
 export default { ResInfo, AllInfo, infoById, infoByTag };
